4a3910eab5dc929501567024442614505cb6b2ee
Julia Kim <26291536+julia-kim@users.noreply.github.com>
2020-12-20 15:31:33 -0500

    Day 08 Part 02

--- src/main/java/days/day08/Day08.java
+++ src/main/java/days/day08/Day08.java
@@ -3,14 +3,15 @@ package days.day08;
 import days.Day;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 
 public class Day08 {
     public int part01(String[] input) {
         int accumulator = 0;
         LinkedHashMap<Integer, String> tracker = new LinkedHashMap<>();
-        int i = 0;
 
+        int i = 0;
         while (i < input.length) {
             String[] instruction = input[i].split(" ");
             String operation = instruction[0];
@@ -34,15 +35,65 @@ public class Day08 {
 
         return accumulator;
     }
-//
-//    public int part02() {
-//        return 1;
-//    }
+
+    public int part02(String[] input) {
+        int accumulator = 0;
+        LinkedHashMap<Integer, String> tracker = new LinkedHashMap<>();
+        HashMap<String, String> operationMap = new HashMap<>();
+        operationMap.put("nop", "jmp");
+        operationMap.put("jmp", "nop");
+
+        boolean gameIsBroken = true;
+        while (gameIsBroken) {
+            accumulator = 0;
+            LinkedHashMap<Integer, String> infiniteLoopTracker = new LinkedHashMap<>();
+            for (int j = 0; j < input.length; j++) {
+                if (tracker.containsKey(j)) {
+                    input[j] = tracker.get(j);
+                } else if (operationMap.containsKey(input[j].substring(0, 3))) {
+                    tracker.put(j, input[j]);
+                    input[j] = input[j].replace(input[j].substring(0, 3), operationMap.get(input[j].substring(0, 3)));
+                    break;
+                }
+            }
+
+            int i = 0;
+            while (i < input.length) {
+                String[] instruction = input[i].split(" ");
+                String operation = instruction[0];
+                int argument = Integer.parseInt(instruction[1]);
+
+                if (infiniteLoopTracker.containsKey(i)) {
+                    break;
+                }
+
+                infiniteLoopTracker.put(i, input[i]);
+
+                if (infiniteLoopTracker.containsKey(input.length - 1)) {
+                    gameIsBroken = false;
+                }
+
+                switch (operation) {
+                    case "acc":
+                        accumulator += argument;
+                        i++;
+                        break;
+                    case "jmp":
+                        i += argument;
+                        break;
+                    case "nop":
+                        i++;
+                        break;
+                }
+            }
+        }
+        return accumulator;
+    }
 
     public static void main(String[] args) throws IOException {
         String[] input = Day.loadInput("day08");
         Day08 day08 = new Day08();
         System.out.println(day08.part01(input));
-        //System.out.println(day08.part02(input));
+        System.out.println(day08.part02(input));
     }
 }
